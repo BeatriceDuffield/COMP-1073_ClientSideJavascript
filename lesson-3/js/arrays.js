5ae6f57c075d574f7776aa925a594f6fdf6110ca
Beatrice Duffield <200522769@student.georgianc.on.ca>
2025-05-20 11:45:38 -0400

    completed lesson files

--- lesson-3/js/arrays.js
+++ lesson-3/js/arrays.js
@@ -1,29 +1,56 @@
-const output = document.querySelector('body p:nth-of-type(2)');
-
+//const output = document.querySelector('body p:nth-of-type(2)');
+const output = document.querySelector('ul');
 /* STEP 1: Creating an array
 When declaring and initializing an array, you can include strings, numbers, booleans, and even other arrays */
-
+let myArray = ["string", true, 100, [5, "hello"]];
+output.textContent = myArray;
 /* STEP 2: Reading and changing array elements
 You can refer to a particular element in an array with it's index number */
-
+output.textContent = `The first element of the array inside myArray is ${myArray[3][0]}`;
 // You can also change a particular element
-
+myArray[1] = false;
 // An array within an array is called a multidimensional array - it can be accessed by specifying the index of the first array, then the item within it
+myArray[3][1] = "goodbye";
 /* STEP 3: Determining array length
 Being able to figure out how many elements are contained in an array is a critical feature of JavaScript programming */
+let totalThingsInArray = myArray.length + myArray[3].length;
+output.textContent = `there are ${myArray.length} elements in myArray`;
+output.textContent = `there are a total of ${totalThingsInArray} elements in myArray`;
 
+output.textContent = "";
 // In particular, looping through arrays
+for (let i = 0; i < myArray.length; i++) {
+    let listItem = document.createElement("li");
+    output.append(listItem);    
 
+    //check if element is an array    
+    if(Array.isArray(myArray[i])){
+        let childList = document.createElement("ul");
+        listItem.append(childList);
+        //we have an array so we need to loop inside that array
+        for (let j = 0; j < myArray[i].length; j++) {
+            let childItem = document.createElement("li");
+            childList.append(childItem);
+            childItem.textContent = `${myArray[i][j]}`;
+        };
+    } else {
+    listItem.textContent = `${myArray[i]}`;
+    };
+};
 /* STEP 4: Convert a string to an array
 If there is a common character that can act as a delimiter in a string, we can use this character to create an array */
+let myString = `Toronto Maple Leafs, Chicago Black Hawks, Detroit Red Wings, Boston Bruins, Montreal Canadians, New York Rangers`;
 
+let stringArray = myString.split(", ");
 // Output one of the array items
-
+output.textContent = stringArray[0];
 // Output the last element of the array
-
+console.log(`last array element is ${stringArray[stringArray.length - 1]}`);
 /* STEP 5: Convert an array back to a string
 Use join() and toString() - note that join() allows you to choose and insert a delimiter, while toString() does not */
-
+    //let stringArrayString = stringArray.toString();
+let stringArrayString = stringArray.join(" | ");
+console.log(stringArrayString);
 /* STEP 6: Adding and removing items from an array
 Without the ability to edit the contents of an array, this type of variable would have limited use - but adding and removing array items is pretty straightforward */
 
